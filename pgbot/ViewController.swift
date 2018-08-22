4061e428b14be85529316820acf91083bc87b14d
Nick Wang <dearxiaowangyu@126.com>
2018-08-22 01:22:17 -0700

    display improved v0.11

--- pgbot/ViewController.swift
+++ pgbot/ViewController.swift
@@ -22,9 +22,9 @@ class ViewController: UIViewController {
     
     func newBot() -> [String] {
         let pha1 = ["Last Choice","Random Choice","Early Choice for All","Player's Choice","Bidding Choice","Deciding Choice"]
-        let pha2 = ["Using Cheapest Resources(Maximum Bid: Minimum Bid + 5 Elektro)","Buys the First Choice for Minimum Bid","Supplying Most Cities(Maximum Bid: Minimum Bid + 10 Elektro)","Highest Number(Maximum Bid: Minimum Bid + # of Own Cities)","Second Smallest Number(Maximum Bid: Minimum Bid)","All Power Plants(Maximum Bid: Minimum Bid + 1 Elektro)"]
-        let pha3 = ["Normal Production and Less Than 5 Elektro", "All Resources","(Last)All Resources, Otherwise Normal Production","Normal Production","Normal Production And Least Available Resources","Odd Turn: Normal Production, Even Turn: All Resources"]
-        let pha4 = ["Last Player Chooses, Cannot Build Through Possible Cities","All Cities, Never More Than First Player","Only Supplied Cities","Step 1: All Cities, Less Than 7; Otherwise: All Cities Never to First Player","Step 1: 1 City, Step 2: 2 Cities, Step 3: 3 Cities","All Cities"]
+        let pha2 = ["Using Cheapest Resources\n(Maximum Bid: Minimum Bid + 5 Elektro)","Buys the First Choice for Minimum Bid","Supplying Most Cities\n(Maximum Bid: Minimum Bid + 10 Elektro)","Highest Number\n(Maximum Bid: Minimum Bid + # of Own Cities)","Second Smallest Number\n(Maximum Bid: Minimum Bid)","All Power Plants\n(Maximum Bid: Minimum Bid + 1 Elektro)"]
+        let pha3 = ["Normal Production and Less Than 5 Elektro", "All Resources","(Last)All Resources,\nOtherwise Normal Production","Normal Production","Normal Production and Least Available Resources","Odd Turn: Normal Production,\nEven Turn: All Resources"]
+        let pha4 = ["Last Player Chooses\n(Cannot Build Through Possible Cities)","All Cities\n(Never More Than First Player)","Only Supplied Cities","Step 1: All Cities, Less Than 7;\nOtherwise: All Cities Never to First Player","Step 1: 1 City, Step 2: 2 Cities, Step 3: 3 Cities","All Cities"]
         let SA = ["Game Start: Gets 100 Elektro","Phase 1: Always Last in Player Order", "Phase 2: Pays Half Bid For Power Plants","Phase 4: All Cities Cost 10 Elektro","Phase 4: Always Builds First City For 0 Elektro","Phase 5: Gets Income for +1 City"]
 
         let ret = [pha1[random(6)],pha2[random(6)],pha3[random(6)],pha4[random(6)],SA[random(6)]]
@@ -47,8 +47,11 @@ class ViewController: UIViewController {
         label.frame = CGRect(x: 0, y: loc,
                            width: UIScreen.main.bounds.width,
                            height: UIScreen.main.bounds.height / 5)
-        print(input)
+        //print(input)
         label.text = input
+        label.lineBreakMode = NSLineBreakMode.byWordWrapping
+        label.numberOfLines = 3
+        label.textAlignment = .center
         label.textColor = blackColor
         label.layer.borderWidth = 2
         label.layer.borderColor = blackColor.cgColor
@@ -58,12 +61,12 @@ class ViewController: UIViewController {
     
     func displayBot(view: UIView, title: [String], content: [String]) {
         for i in 0...4 {
-            buildBox(view:view, number:i, input:title[i]+": "+content[i])
+            buildBox(view:view, number:i, input:title[i]+": \n"+content[i])
         }
     }
     
     func makeABot(view: UIView) {
-        var content = newBot()
+        let content = newBot()
         let title = ["First City", "Phase 1", "Phase 2", "Phase 4", "Special"]
         displayBot(view: view, title: title, content: content)
     }
